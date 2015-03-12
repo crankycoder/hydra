cff8372d64d98fcf7c4212f7d6f61792b420124a
Victor Ng <victor@crankycoder.com>
2015-03-12 12:46:16 -0400

    PEP8 the setup.py and bumped to 2.0

--- setup.py
+++ setup.py
@@ -5,29 +5,29 @@ from os.path import join
 
 import os
 
-ext_modules=[ 
-        Extension("_hydra", 
-            extra_compile_args=['-std=gnu99', '-O2', '-D_LARGEFILE64_SOURCE'],
-            sources = [
-                "src/_hydra.pyx",
-                'src/mmap_writer.c',
-                'src/MurmurHash3.c'],
-            include_dirs = [join(os.getcwd(), 'src')],  # path to .h file(s)
-            library_dirs = [join(os.getcwd(), 'src')],  # path to .a or .so file(s)
-            ),
-]
+ext_modules = [Extension("_hydra",
+                         extra_compile_args=['-std=gnu99',
+                                             '-O2',
+                                             '-D_LARGEFILE64_SOURCE'],
+                         sources=["src/_hydra.pyx",
+                                  'src/mmap_writer.c',
+                                  'src/MurmurHash3.c'],
 
-setup(
-  name = 'Hydra',
-  author='Victor Ng',
-  author_email = 'crankycoder@gmail.com',
-  description = 'A high performance persistent bloom filter',
-  url = "http://bitbucket.org/crankycoder/Hydra",
-  version='1.2',
-  license = 'MIT License',
-  cmdclass = {'build_ext': build_ext},
-  zip_safe=False,
-  package_dir = {'': 'src'},
-  py_modules = ['hydra'],
-  ext_modules = ext_modules,
-)
+                         # path to .h file(s)
+                         include_dirs=[join(os.getcwd(), 'src')],
+
+                         # path to .a or .so file(s)
+                         library_dirs=[join(os.getcwd(), 'src')])]
+
+setup(name='Hydra',
+      author='Victor Ng',
+      author_email='crankycoder@gmail.com',
+      description='A high performance persistent bloom filter',
+      url="http://bitbucket.org/crankycoder/Hydra",
+      version='2.0',
+      license='MIT License',
+      cmdclass={'build_ext': build_ext},
+      zip_safe=False,
+      package_dir={'': 'src'},
+      py_modules=['hydra'],
+      ext_modules=ext_modules)

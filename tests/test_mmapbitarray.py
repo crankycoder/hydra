c3a6f47cd94caf616e0e5a1be279f04f1ce2c61a
Henry Stern <stern@fsi.io>
2015-03-13 10:47:58 -0300

    Fix mmapbitfield unset

--- tests/test_mmapbitarray.py
+++ tests/test_mmapbitarray.py
@@ -10,3 +10,37 @@ def test_ro_segfault():
         ro_field[0] = 1
     except ValueError:
         pass
+
+def test_setitem():
+    tf = tempfile.NamedTemporaryFile(delete=True)
+    bf = MMapBitField(tf.name, 80, 0)
+
+    # verify set once
+    bf[0] = 1
+    assert bf[0]
+    for idx in range(1, len(bf)):
+        assert not bf[idx]
+    bf[0] = 1
+    assert bf[0]
+    for idx in range(1, len(bf)):
+        assert not bf[idx]
+
+    # verify unset twice
+    bf[0] = 0
+    for idx in range(len(bf)):
+        assert not bf[idx]
+    bf[0] = 0
+    for idx in range(len(bf)):
+        assert not bf[idx]
+
+    # verify set at end twice
+    bf[len(bf)-1] = 1
+    assert bf[len(bf)-1]
+    bf[len(bf)-1] = 1
+    assert bf[len(bf)-1]
+
+    # verify unset at end twice
+    bf[len(bf)-1] = 0
+    assert not bf[len(bf)-1]
+    bf[len(bf)-1] = 0
+    assert not bf[len(bf)-1]
